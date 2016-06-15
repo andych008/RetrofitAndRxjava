a28639536aae021852a457535d65db4000100d5d
喵叔catuncle <andych008@163.com>
2016-06-15 15:25:05 +0800

    rx style confirm Dialog

--- app/src/main/java/com/dwgg/retrofitandrxjava/utils/Tools.java
+++ app/src/main/java/com/dwgg/retrofitandrxjava/utils/Tools.java
@@ -1,11 +1,19 @@
 package com.dwgg.retrofitandrxjava.utils;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.text.TextUtils;
 import android.widget.Toast;
+
+import com.dwgg.retrofitandrxjava.R;
+
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * 常用工具方法
@@ -39,6 +47,62 @@ public class Tools {
             }
         }
         return versionName;
+    }
+
+    public static Observable<Boolean> confirmOk(final Activity activity, final String text) {
+        return Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(final Subscriber<? super Boolean> subscriber) {
+                showConfirmOk(activity, text, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        subscriber.onNext(true);
+                        subscriber.onCompleted();
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+    }
+
+    public static Observable<Boolean> confirm(final Activity activity, final String text) {
+        return Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(final Subscriber<? super Boolean> subscriber) {
+                showConfirm(activity, text, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        subscriber.onNext(true);
+                        subscriber.onCompleted();
+                        dialog.dismiss();
+                    }
+                }, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        subscriber.onNext(false);
+                        subscriber.onCompleted();
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+    }
+
+    private static void showConfirmOk(Activity activity, String text, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(activity)
+                .setMessage(text)
+                .setPositiveButton(R.string.ok, okListener)
+                .create()
+                .show();
+    }
+
+    private static void showConfirm(Activity activity, String text, DialogInterface.OnClickListener okListener, DialogInterface.OnClickListener cancelListener) {
+        new AlertDialog.Builder(activity)
+                .setMessage(text)
+                .setPositiveButton(R.string.ok, okListener)
+                .setNegativeButton(R.string.cancel, cancelListener)
+                .create()
+                .show();
     }
 
     private static void requireInited() {
